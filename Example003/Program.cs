a4ad3b84998317dd20145c3a0ab6f077e25ec7c9
Nata <muravevantsh@mail.ru>
2022-08-26 15:56:52 +0300

    Example002

---
+++ Example003/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int year = int.Parse(Console.ReadLine() ?? "0");
+
+void NextDiffYear(int year)
+{
+    for (int y = year + 1; ; y++)
+    {
+        int a = y / 1000;
+        int b = y / 100 % 10;
+        int c = y / 10 % 10;
+        int d = y % 10;
+        if (a != b && b != c && c != d && a != c && a != d && b != d)
+        {
+          Console.WriteLine(y); 
+            return;
+        }
+    }
+}
+NextDiffYear(year);
+
---
+++ Example004/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число для проверки: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+bool k = false; 
+for(int i=0; i < Digitsnumber(n); i++)
+{    
+    if((Digitindex(n,i+1) == 4 || Digitindex(n,i+1) == 7)
+        &&(Digitsnumber(n) == 4 || Digitsnumber(n) == 7))
+        k = true;
+}
+if(k == true)
+{
+    Console.WriteLine("Да, введенное Вами число является счастливым.");
+}
+else
+{
+    Console.WriteLine("Нет, введенное Вами число не является счастливым.");
+}
+;
